c09ba69fa61811e45597bb8f9f73f2eb25ef73a0
danielzheng1 <danz0219@gmail.com>
2017-08-24 09:30:36 -0700

    Third time's a charm :-)

---
+++ Project2/src/Project2.java
@@ -0,0 +1,13 @@
+//Daniel Zheng
+/* August 24, 2017
+This class compares output using print and pintln
+ */
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
