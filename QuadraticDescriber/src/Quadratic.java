81300aab05cb8436c170e06e808ad5e81ccf19fc
danielzheng1 <31107122+danielzheng1@users.noreply.github.com>
2017-10-10 10:33:20 -0700

    Part 5

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -3,11 +3,11 @@ public class Quadratic {
 	public static String quadrDescriber(double a, double b, double c) {
 		System.out.println("Description of the graph of:\n" + "y = " + a + " x^2 + " + b + " x + " + c + "\n");
 		String open = "Opens: ";
-		String aos = "Axis of Symmetry: " + (-b / (2 * a) + "\n");
-		double xvertex = -b / (2 * a);
-		double yvertex = (a * (xvertex * xvertex)) + (b * xvertex) + c;
+		double aos = round2(-b / (2 * a));
+		double xvertex = round2(-b / (2 * a));
+		double yvertex = round2((a * (xvertex * xvertex)) + (b * xvertex) + c);
 		String xroots = ("x-intercept(s): " + quadForm(a,b,c) + "\n");
-		String yroots = ("y-intercept: (0, " + c + ")\n");
+		String yroots = ("y-intercept: " + round2(c) + "\n");
 		String vertex = "Vertex: (" + xvertex + ", " + yvertex+ ")\n";
 		
 		if (a > 0) {
@@ -16,7 +16,7 @@ public class Quadratic {
 			open += "Down\n";
 		}
 		
-		return  open + aos + vertex + xroots + yroots ;
+		return  open + "Axis of Symmetry: " + aos + "\n" + vertex + xroots + yroots ;
 	}
 	public static double round2(double numero) {
 		
@@ -47,9 +47,9 @@ public class Quadratic {
 			return "None";
 		}
 		else if(discriminant(a,b,c) > 0) {
-			double max = round2((-b + sqrt(discriminant(a,b,c))/(2 * a)));
-			double min = round2((-b - sqrt(discriminant(a,b,c))/(2 * a)));
-			return "roots at " + min + " and " + max;
+			double max = round2(((-b + sqrt(discriminant(a,b,c)))/(2 * a)));
+			double min = round2(((-b - sqrt(discriminant(a,b,c)))/(2 * a)));
+			return min + " and " + max;
 		}
 		else {
 			return "(" + -b / (2 * a) + ", 0)";
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 public class QuadraticClient {
 	public static void main(String[] args) {
 	Scanner userInput = new Scanner(System.in);
-	String response = "";
+	System.out.println("Welcome to Quadratic Describer!");
+	System.out.println("Please input your 3 coefficients:");
+	boolean done = false;
 	do {
-		System.out.println("Please input your 3 coefficients:");
 		System.out.print("a: ");
 	double a = userInput.nextDouble();
 		System.out.print("b: ");
@@ -16,7 +17,12 @@ public class QuadraticClient {
 		System.out.println();
 		System.out.println(Quadratic.quadrDescriber(a, b, c));
 		System.out.println("Do you want to keep going? (Type \"quit\" to end)");
-	response = userInput.next();
-	} while(!response.equals("quit"));//fix
+	String finished = userInput.next();
+	char answer = finished.charAt(0);
+	if (answer == 'q') {
+		done = true;
 	}
+	System.out.println();
+	} while (done == false);
+}
 }
