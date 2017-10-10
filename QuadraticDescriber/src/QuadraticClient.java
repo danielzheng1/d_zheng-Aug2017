75d08aca3a07d4a144404c22ee5994259899194e
danielzheng1 <31107122+danielzheng1@users.noreply.github.com>
2017-10-10 10:33:38 -0700

    Part 5

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -2,27 +2,27 @@ import java.util.Scanner;
 //Daniel Zheng
 //10/6/17
 public class QuadraticClient {
-	public static void main(String[] args) {
-	Scanner userInput = new Scanner(System.in);
-	System.out.println("Welcome to Quadratic Describer!");
-	System.out.println("Please input your 3 coefficients:");
-	boolean done = false;
-	do {
-		System.out.print("a: ");
-	double a = userInput.nextDouble();
-		System.out.print("b: ");
-	double b = userInput.nextDouble();
-		System.out.print("c: ");
-	double c = userInput.nextDouble();
+		public static void main(String[] args) {
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("Welcome to Quadratic Describer!");
+			System.out.println("Please input your 3 coefficients:");
+		boolean done = false;
+		do {
+			System.out.print("a: ");
+		double a = userInput.nextDouble();
+			System.out.print("b: ");
+		double b = userInput.nextDouble();
+			System.out.print("c: ");
+		double c = userInput.nextDouble();
+			System.out.println();
+			System.out.println(Quadratic.quadrDescriber(a, b, c));
+			System.out.println("Do you want to keep going? (Type \"quit\" to end)");
+		String finished = userInput.next();
+		char answer = finished.charAt(0);
+		if (answer == 'q') {
+			done = true;
+		}
 		System.out.println();
-		System.out.println(Quadratic.quadrDescriber(a, b, c));
-		System.out.println("Do you want to keep going? (Type \"quit\" to end)");
-	String finished = userInput.next();
-	char answer = finished.charAt(0);
-	if (answer == 'q') {
-		done = true;
+		} while (done == false);
 	}
-	System.out.println();
-	} while (done == false);
-}
 }
